7e45f1e95c0c00716819768a9b93f46292c40a55
Joel Maldonado <strange.cute.loop@gmail.com>
2026-02-19 17:39:05 -0700

    config wars

--- ece569/build_dir/configure_hw2.sh
+++ ece569/build_dir/configure_hw2.sh
@@ -5,12 +5,29 @@ module load cuda11/11.0
 module load cmake/3.28.3
 
 export CC=gcc
+export CXX=g++
 
 cd /home/u16/joelmaldonado/HPC-GPU/ece569/build_dir
+
+ASSIGNMENT2_DIR=/home/u16/joelmaldonado/HPC-GPU/Assignment2
+LIBWB_TARGET="$ASSIGNMENT2_DIR/libwb"
+LIBWB_SOURCE=/home/u16/joelmaldonado/HPC-GPU/ece569/labs/libwb
+
+if [[ ! -f "$LIBWB_TARGET/sources.cmake" ]]; then
+	if [[ -f "$LIBWB_SOURCE/sources.cmake" ]]; then
+		ln -sfn "$LIBWB_SOURCE" "$LIBWB_TARGET"
+	else
+		echo "ERROR: libwb not found. Expected one of:"
+		echo "  $LIBWB_TARGET/sources.cmake"
+		echo "  $LIBWB_SOURCE/sources.cmake"
+		echo "Copy/provision libwb first, then rerun this script."
+		exit 1
+	fi
+fi
 
 rm -f CMakeCache.txt Makefile cmake_install.cmake compile_commands.json
 rm -rf CMakeFiles
 
-cmake -D CUDA_TOOLKIT_ROOT_DIR=/opt/ohpc/pub/apps/cuda/11.8 ../../Assignment2
+cmake -D CUDA_TOOLKIT_ROOT_DIR=/opt/ohpc/pub/apps/cuda/11.8 "$ASSIGNMENT2_DIR"
 
 echo "Configuration complete. Next step: run 'make -j' on HPC when ready."
